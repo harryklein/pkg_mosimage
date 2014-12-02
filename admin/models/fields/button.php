291286a4c6c5a0a81dd90fe6f48cbf7775adbefa
Harry Klein <harry.klein@joomla-hklein.de>
2014-12-02 23:06:10 +0100

    Refaktoring und Reformating

--- admin/models/fields/button.php
+++ admin/models/fields/button.php
@@ -1,89 +1,101 @@
-<?php defined('_JEXEC') or die('Restricted access');
+<?php
 
 /**
+ *
  * @version 2.0 $Id: button.php,v 1.3 2014-10-30 21:50:08 harry Exp $
  * @package Joomla
  * @subpackage H2N Mosimage Component
- * @copyright (C) 2010 Harry Klein - www.joomla-hklein.de
- * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
- * @license GNU/GPL, see LICENSE.php
- *
- * H2N Mosimage Component is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * H2N Mosimage Component is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with EventList; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * @copyright (C) 2010-2014 Harry Klein - www.joomla-hklein.de
+ * @license GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
 
 /**
- *  
-<pre>
-<field name="up" type="button" label="Up"   onclick="Mosimage.moveInList('adminForm','jform_images',-1)" disabled="true" class="test" hidden="true"/>
-</pre>
-*/
-class JFormFieldButton extends JFormField {
+ * Erlaubt es, eine Button via xml-Beschreibung in ein Form einzubinden.
+ * <ul>
+ * <li>name: </li>
+ * <li>type: button</li>
+ * <li>label: Text, der auf dem Button stehen soll</li>
+ * <li>description: Text, der als Tooltip dargestellt werden soll
+ * <li>onclick: JS, welches beim Kick auf den Button ausgeführt werden soll</li>
+ * <li>disable: Bei true ist der Button nicht klickbar, wird aber dargestellt</li>
+ * <li>hidden: Bei true wird der Button nicht angezeigt</li>
+ * <li>class: CSS-Klasse, default ist btn</li>
+ * <li>size: Größe des Buttons in em. Ist das Label länger als das Button, so wird das Label rechts abgeschnitten. Ohne Angabe einer Größe ist der Button so groß wie nötig.
+ * </ul>
+ * Beispiel XML
+ * <pre>
+ <field name="up" type="button" label="Up" description="Up desc"
+ onclick="Mosimage.moveInList('adminForm','jform_images',-1)"
+ disabled="true" class="test" />
+ </pre>
+ * Als HTML wird ausgegebn:
+ <pre>
+ <input type="button" disabled="disabled" title="Up desc"
+ onclick="Mosimage.moveInList('adminForm','jform_images',-1)" value="Up" class="test" id="jform_up">
+ </pre>
+ */
+class JFormFieldButton extends JFormField
+{
 
-	protected $type = 'Button';
+    protected $type = 'Button';
 
-	protected function getInput(){
-		
-		if ($this->hidden) {
-			return '';
-		}
-		
-		$label = $this->element['label'] ? (string) $this->element['label'] : (string) $this->element['name'];
-		$label = $this->translateLabel ? JText::_($label) : $label;
-		
-		if (!empty($this->description)){
-			$description = $this->translateDescription ? JText::_($this->description) : $this->description;
-		} else {
-			$description = '';
-		}
-		
-		if ($this->disabled){
-			$disabled = ' disabled="disabled" ';
-		} else {
-			$disabled = '';
-		}
-		
+    public function __construct ($form = null)
+    {
+        parent::__construct($form);
+    }
 
-		if ($this->class){
-			$class = ' class="' . $this->class . '" ';
-		} else {
-			$class = ' class="btn" ';
-		}
-		
-		$value =   ' value="'   . $label .  '" ';
-		$onclick = ' onclick="' . $this->onclick  . '" ';
-		$title =   ' title="'   . $description . '"';
-		$id    =   ' id="' . $this->id . '"'; 
-		$name  =   ' name="' . $this->name . '"';
-		
-		$option = '<input type="button" '
-				. $id
-				. $class
-				. $value 
-				. $onclick
-				. $title 
-				. $disabled		
-				. '/>'
-				;
-		return $option;
-		
-	}
-	
-	protected function getLabel(){
-		return '&nbsp;';
-	}
+    private function prepareAttribute ($attribute, $value)
+    {
+        $result = sprintf('%s="%s"', $attribute, $value);
+        return $result;
+    }
+
+    protected function getInput ()
+    {
+        $attribute = array();
+        if ($this->hidden) {
+            $type = 'hidden';
+        } else {
+            $type = 'button';
+        }
+        
+        $attribute['type'] = $this->prepareAttribute('type', $type);
+        
+        $label = $this->element['label'] ? (string) $this->element['label'] : (string) $this->element['name'];
+        $label = $this->translateLabel ? JText::_($label) : $label;
+        $attribute['value'] = $this->prepareAttribute('value', $label);
+        
+        if (! empty($this->description)) {
+            $description = $this->translateDescription ? JText::_($this->description) : $this->description;
+            $attribute['title'] = $this->prepareAttribute('title', $description);
+        }
+        
+        if ($this->disabled) {
+            $attribute['disable'] = $this->prepareAttribute('disabled', 'disabled');
+        }
+        
+        if ($this->class) {
+            $attribute['class'] = $this->prepareAttribute('class', $this->class);
+        } else {
+            $attribute['class'] = $this->prepareAttribute('class', "btn");
+        }
+        
+        if (isset($this->size) && (((int) $this->size) > 0)) {
+            $attribute['size'] = 'style="width:' . $this->size . 'em;"';
+        }
+        $attribute['onclick'] = $this->prepareAttribute('onclick', $this->onclick);
+        $attribute['id'] = $this->prepareAttribute('id', $this->id);
+        $attribute['name'] = $this->prepareAttribute('name', $this->name);
+        
+        $option = '<input ' . join(' ', $attribute) . '/>';
+        return $option;
+    }
+
+    protected function getLabel ()
+    {
+        return '&nbsp;';
+    }
 }
-
-
 
 ?>
