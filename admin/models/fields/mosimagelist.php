519d23c066ed8425a0522d60203831dfad5a70fe
Harry Klein <harry.klein@joomla-hklein.de>
2014-12-10 17:54:04 +0100

    Refactoring: Variablen umbenannt und Code extrahiert-

--- admin/models/fields/mosimagelist.php
+++ admin/models/fields/mosimagelist.php
@@ -1,77 +1,78 @@
-<?php defined('_JEXEC') or die('Restricted access');
-
+<?php
 /**
  * @version 2.0 $Id: mosimagelist.php,v 1.3 2014-10-30 21:50:49 harry Exp $
  * @package Joomla
  * @subpackage H2N Mosimage Component
- * @copyright (C) 2010 Harry Klein - www.joomla-hklein.de
- * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
- * @license GNU/GPL, see LICENSE.php
- *
- * H2N Mosimage Component is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * H2N Mosimage Component is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with EventList; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/ 
+ * @copyright (C) 2010-2014 Harry Klein - www.joomla-hklein.de
+ * @license GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
 
 JFormHelper::loadFieldClass('list');
 
 /**
+ * Die Befüllung des Elements ist nicht offentsichtlich. getItem des Modells liefert ein
+ * Objekt item, welches das Attribut imageslist enthält. 
+ * 
+ * Im Form gibt es das korrespondierende Element:
+ * <field id="imageslist" name="imageslist" type="mosimagelist" size="10" ...
  *
-*/
-class JFormFieldMosimageList extends JFormFieldList {
+ * Beim Befüllen des Form wird value mit dem Wert aus item->imageslist befüllt und 
+ * bei getOption entsprechend ausgewertet. Nach des Auwertung wird value auf den selectierten Eintrag (also der oberste) 
+ * gesetzt. Nicht schön, abes es geht.
+ */
+class JFormFieldMosimageList extends JFormFieldList
+{
 
-	protected $type = 'MosmageList';
+    protected $type = 'MosmageList';
+    
+    public function __construct ($form = null)
+    {
+        parent::__construct($form);
+    }
 
+    protected function getInput ()
+    {
+        $baseURL = JURI::root() . '/images/';
+        
+        $a1 = ' onkeyup="' . "Mosimage.showImageProps( '" . $baseURL . "' );";
+        $a2 = ' onclick="' . "Mosimage.showImageProps( '" . $baseURL . "' );";
+        
+        // wir misbrauchen hier onchange, da Fieldlist kein onclick und onkeyup unterstützt
+        $this->onchange = '" ' . $a1 . '"' . $a2;
+        return parent::getInput();
+    }
 
-	protected function getInput(){
-		$baseURL = JURI::root() .'/images/';
-		
-		$a1 = ' onkeyup="' . "Mosimage.showImageProps( '". $baseURL ."' );";
-		$a2 = ' onclick="' . "Mosimage.showImageProps( '". $baseURL ."' );";
-		 
-		// wir misbrauchen hier onchange, da Fieldlist kein onclick und onkeyup unterstützt
-		$this->onchange = '" ' . $a1 . '"' . $a2;
-		return parent::getInput();
-	}
-
-
-	protected function getOptions() {
-		$value = $this->value;
-		$options= array();
-		$i = 0;
-		
-		// wir machen aus dem Json wieder ein Objekt
-		$jsonObjects = json_decode($value,false);
-		
-		
-		foreach ($jsonObjects as $obj){
-			$data = json_encode($obj);
-			$source = $obj->source;
-			if( strrchr($source, '/') ) {
-				$filename = substr( strrchr($source, '/' ), 1 );
-			} else {
-				$filename = $source;
-			}
-			$tmp = JHTML::_('select.option', $data, $filename );
-			$options[] = $tmp;
-			// 1. Zeile selektrieren
-			if  ($i == 0){
-				$this->value = $data;
-			}
-			$i++;
-		}
-		return $options;
-	}
-
-
+    protected function getOptions ()
+    {
+        $value = $this->value;
+        $options = array();
+        $i = 0;
+        
+        $jsonAsObjects = json_decode($value, false);
+        
+        foreach ($jsonAsObjects as $obj) {
+            $imageAttribuesAsJson = json_encode($obj);
+            $filename = $this->stripTrailingSlashFromFilename($obj->source);
+            $options[] = JHTML::_('select.option', $imageAttribuesAsJson, $filename);
+            // 1. Zeile selektrieren
+            if ($i == 0) {
+                $this->value = $imageAttribuesAsJson;
+            }
+            $i ++;
+        }
+        return $options;
+    }
+    
+    
+    private function stripTrailingSlashFromFilename($source){
+        if (strrchr($source, '/')) {
+            $filename = substr(strrchr($source, '/'), 1);
+        } else {
+            $filename = $source;
+        }
+        return $filename;
+    }
+    
 }
 ?>
