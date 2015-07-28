274193c26b4846a64e461f3182ae0131eb5e9236
Harry Klein <harry.klein@joomla-hklein.de>
2015-07-28 09:43:43 +0200

    build.xml wird jetzt zentral verwendet

--- bin/ant.sh
+++ bin/ant.sh
@@ -2,8 +2,6 @@
 
 ROOT_DIR=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))
 >&2 echo "ROOT_DIR ....: [${ROOT_DIR}]"
-
 cd ${ROOT_DIR}
 
-export VERSION=$(./bin/build.sh -b)
-ant -lib ${HOME}/ant-joomla/lib -DVERSION=${VERSION} -f bin/build.xml $@
+ant -f bin/build.xml $@
