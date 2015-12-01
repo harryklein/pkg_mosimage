6e2321cd9dc2af203dfc1671b77402a193a40e00
Harry Klein <harry.klein@joomla-hklein.de>
2015-12-01 23:51:40 +0100

    CVS-Id entfernt

--- site/mosimage.php
+++ site/mosimage.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @version 2.0 $Id: mosimage.php,v 1.1 2014/12/16 23:37:44 harry Exp $
  * @package Joomla.Site
  * @subpackage com_mosimage
  * @copyright (C) 2010-2014 Harry Klein - www.joomla-hklein.de
